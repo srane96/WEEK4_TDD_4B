38c2dd03b0c21c7989da35958154bcd0a5065a61
srane96 <srane96@terpmail.umd.edu>
2018-09-22 18:04:34 -0400

    Edit CmakeList and test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,24 +1,26 @@
 #include <gtest/gtest.h>
-#include <memory>
 #include "pid.h"
 
-std::shared_ptr<PID> dummyPID;
-
+/**
+ * Test class initialization
+ */
 TEST(PIDTest, classInitialization) {
-  dummyPID = std::make_shared<PID>();
-//  dummyPID->setParameters(1.5, 10.2, 25.2);
+  PID dummyPID;
+  dummyPID.setParameters(1.5, 10.2, 25.2);
 
-  EXPECT_EQ(1.5, dummyPID->getKp());
-  EXPECT_EQ(10.2, dummyPID->getKi());
-  EXPECT_EQ(25.2, dummyPID->getKd());
+  EXPECT_EQ(1.5, dummyPID.getKp());
+  EXPECT_EQ(10.2, dummyPID.getKi());
+  EXPECT_EQ(25.2, dummyPID.getKd());
 }
+/**
+ * Test compute method
+ */
+TEST(PIDTest, computeControlInput) {
 
-//TEST(PIDTest, computeControlInput) {
-//
-//  PID dummyPID(1.5, 10.2, 25.2);
-//
-//  double setPoint = 5.0;
-//  double currentVelocity = 2.0;
-//
-//  EXPECT_NEAR(7564.806, dummyPID.compute(setPoint, currentVelocity), 10.0);
-//}
+  PID dummyPID(1.5, 10.2, 25.2);
+
+  double setPoint = 5.0;
+  double currentVelocity = 2.0;
+
+  EXPECT_NEAR(7564.806, dummyPID.compute(setPoint, currentVelocity), 10.0);
+}
