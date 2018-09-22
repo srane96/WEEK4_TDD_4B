9f31866511a4f8a2b02e5574545dea138bbe7fb3
srane96 <srane96@terpmail.umd.edu>
2018-09-22 19:25:41 -0400

    Update test case

--- test/test.cpp
+++ test/test.cpp
@@ -6,21 +6,21 @@
  */
 TEST(PIDTest, classInitialization) {
   PID dummyPID;
-  dummyPID.setParameters(1.5, 10.2, 25.2);
+  dummyPID.setParameters(15.0, 50.5, 2.0);
 
-  EXPECT_EQ(1.5, dummyPID.getKp());
-  EXPECT_EQ(10.2, dummyPID.getKi());
-  EXPECT_EQ(25.2, dummyPID.getKd());
+  EXPECT_EQ(15.0, dummyPID.getKp());
+  EXPECT_EQ(50.0, dummyPID.getKi());
+  EXPECT_EQ(25.0, dummyPID.getKd());
 }
 /**
  * Test compute method
  */
 TEST(PIDTest, computeControlInput) {
 
-  PID dummyPID(1.5, 10.2, 25.2);
+  PID dummyPID(20.2, 30.3, 1.1);
 
   double setPoint = 5.0;
-  double currentVelocity = 2.0;
+  double currentVelocity = 2.5;
 
-  EXPECT_NEAR(7564.806, dummyPID.compute(setPoint, currentVelocity), 10.0);
+  EXPECT_NEAR(401.25, dummyPID.compute(setPoint, currentVelocity), 10.0);
 }
