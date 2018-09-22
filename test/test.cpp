9f285691a669f37604368c8e4f1b0e660d6dbcc6
srane96 <srane96@terpmail.umd.edu>
2018-09-22 17:05:55 -0400

    Add source file for google test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,21 @@
 #include <gtest/gtest.h>
+#include "pid.h"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+TEST(PIDTest, classInitialization) {
+
+  PID dummyPID(1.5, 10.2, 25.2);
+
+  EXPECT_EQ(1.5, dummyPID.getKp());
+  EXPECT_EQ(10.2, dummyPID.getKi());
+  EXPECT_EQ(25.2, dummyPID.getKd());
+}
+
+TEST(PIDTest, computeControlInput) {
+
+  PID dummyPID(1.5, 10.2, 25.2);
+
+  double setPoint = 5.0;
+  double currentVelocity = 2.0;
+
+  EXPECT_NEAR(7564.806, dummyPID.compute(setPoint, currentVelocity), 10.0);
 }
